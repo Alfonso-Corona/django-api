6fe75f851750a20c65cb0d0ebf4098c5d3f48c04
Alfonso Corona <coronaalfonsor@gmail.com>
2025-01-13 13:40:19 -0600

    feat: setting up jwt

--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +35,6 @@ urlpatterns = [
     path('orders/', views.OrderListAPIView.as_view()),
     path('silk/', include('silk.urls', namespace='silk')),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
